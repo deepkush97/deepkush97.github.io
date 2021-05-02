127f3afddef86df4e3f9ab3a1f0af1868d64a0ec
Deepanshu Kushwaha <kushwaha97deepanshu@gmail.com>
2021-05-02 21:02:44 +0530

    Added: SideBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { NavBar } from "./components/NavBar";
+import { SideBar } from "./components/SideBar";
 export const App = () => {
   return (
     <>
       <NavBar />
+      <div className="d-flex">
+        <SideBar />
+        <main></main>
+      </div>
     </>
   );
 };
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,47 @@
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+
+export const SideBar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sticky-top">
+        <div className="sidebar-content">
+          <a href="mailto:kushwaha97deepanshu@gmail.com" className="logo-link">
+            <OverlayTrigger
+              placement="right"
+              overlay={<Tooltip id="mail-tooltip">Mail</Tooltip>}
+            >
+              <i className="far fa-envelope fa-2x"></i>
+            </OverlayTrigger>
+          </a>
+          <a href="https://github.com/deepkush97" className="logo-link">
+            <OverlayTrigger
+              placement="right"
+              overlay={<Tooltip id="github-tooltip">Github</Tooltip>}
+            >
+              <i className="fab fa-github fa-2x"></i>
+            </OverlayTrigger>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/leftisfame/"
+            className="logo-link"
+          >
+            <OverlayTrigger
+              placement="right"
+              overlay={<Tooltip id="linkedin-tooltip">LinkedIn</Tooltip>}
+            >
+              <i className="fab fa-linkedin fa-2x"></i>
+            </OverlayTrigger>
+          </a>
+          <a href="https://www.instagram.com/leftisfame" className="logo-link">
+            <OverlayTrigger
+              placement="right"
+              overlay={<Tooltip id="instagram-tooltip">Instagram</Tooltip>}
+            >
+              <i className="fab fa-instagram fa-2x"></i>
+            </OverlayTrigger>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
