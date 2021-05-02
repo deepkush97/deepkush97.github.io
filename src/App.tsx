7eebd37986a0e7f05181b94d4d3533a93c167bb5
Deepanshu Kushwaha <deepanshu@atliq.com>
2021-05-02 22:53:00 +0530

    Added: react-router and updated links in NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { NavBar } from "./components/NavBar";
 import { SideBar } from "./components/SideBar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
 export const App = () => {
   return (
-    <>
+    <Router>
       <NavBar />
       <div className="d-flex">
         <SideBar />
-        <main></main>
+        <main>
+          <Route path="/" component={HomePage} />
+        </main>
       </div>
-    </>
+    </Router>
   );
 };
