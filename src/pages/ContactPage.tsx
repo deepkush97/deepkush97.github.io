2100d738f8fba1cd3d9bec9418c31340f1bf05bc
Deepanshu Kushwaha <deepanshu@atliq.com>
2021-05-03 03:05:54 +0530

    Added: ContactPage with SocialMediaCard

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,5 +1,39 @@
 import React from "react";
+import { Row } from "react-bootstrap";
+import { PageLayout } from "../components/PageLayout";
+import { SocialMediaCard } from "../components/SocialMediaCard";
 
 export const ContactPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <PageLayout title="Get in touch">
+        <Row className="align-items-stretch">
+          <SocialMediaCard
+            description="To have a formal conversation or to share something with me."
+            url="mailto:kushwaha97deepanshu@gmail.com"
+            iconClass="far fa-envelope"
+            linkText="Mail me!"
+          />
+          <SocialMediaCard
+            description="To see my projects, my repository and my personal work."
+            url="https://github.com/deepkush97"
+            iconClass="fab fa-github"
+            linkText="Visit me!"
+          />
+          <SocialMediaCard
+            description="To connect to me professionally and build network."
+            url="https://www.linkedin.com/in/leftisfame/"
+            iconClass="fab fa-linkedin"
+            linkText="Connect with me!"
+          />
+          <SocialMediaCard
+            description="To get to know me personally. I am another person in real."
+            url="https://www.instagram.com/leftisfame"
+            iconClass="fab fa-instagram"
+            linkText="Follow me!"
+          />
+        </Row>
+      </PageLayout>
+    </>
+  );
 };
