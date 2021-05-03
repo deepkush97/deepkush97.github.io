b86a452c856ad34b65d8e43428f7d6292efadde3
Deepanshu Kushwaha <kushwaha97deepanshu@gmail.com>
2021-05-03 23:56:13 +0530

    Added: ResumePage, with educational and experience

---
+++ src/components/ModalWrapper.tsx
@@ -0,0 +1,35 @@
+import { FC } from "react";
+import { Button, Modal } from "react-bootstrap";
+
+interface ModalWrapperProps {
+  show: boolean;
+  onHide: () => void;
+  modalId: string;
+  title: string;
+}
+
+export const ModalWrapper: FC<ModalWrapperProps> = ({
+  show,
+  onHide,
+  modalId,
+  title,
+  children,
+}) => {
+  return (
+    <Modal
+      show={show}
+      onHide={onHide}
+      size="lg"
+      aria-labelledby={modalId}
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id={modalId}>{title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{children}</Modal.Body>
+      <Modal.Footer>
+        <Button onClick={onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -1,5 +1,71 @@
-import React from "react";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+import { PageLayout } from "../components/PageLayout";
+import { EducationModalBody } from "../components/EducationModalBody";
+import { ExperienceModalBody } from "../components/ExperienceModalBody";
+import { SkillsModalBody } from "../components/SkillsModalBody";
+import { ModalWrapper } from "../components/ModalWrapper";
 
 export const ResumePage = () => {
-  return <div></div>;
+  const [skillsModalOpen, setSkillsModalOpen] = useState(false);
+  const [experienceModalOpen, setExperienceModalOpen] = useState(false);
+  const [educationModalOpen, setEducationModalOpen] = useState(false);
+
+  return (
+    <PageLayout title="Resume">
+      <div className="d-flex flex-column px-3">
+        <Button
+          className=" btn-outline-link mb-2 p-4"
+          onClick={() => setSkillsModalOpen(true)}
+        >
+          <>
+            <div className="display-4"> Skills </div>
+            <i className="fas fa-shapes fa-3x"></i>
+          </>
+        </Button>
+        <ModalWrapper
+          show={skillsModalOpen}
+          onHide={() => setSkillsModalOpen(false)}
+          modalId="skills-modal"
+          title="Skills"
+        >
+          <SkillsModalBody />
+        </ModalWrapper>
+        <Button
+          className=" btn-outline-link mb-2 p-4"
+          onClick={() => setExperienceModalOpen(true)}
+        >
+          <>
+            <div className="display-4"> Experience </div>
+            <i className="fas fa-user-graduate fa-3x"></i>
+          </>
+        </Button>
+        <ModalWrapper
+          show={experienceModalOpen}
+          onHide={() => setExperienceModalOpen(false)}
+          modalId="experience-modal"
+          title="Experience"
+        >
+          <ExperienceModalBody />
+        </ModalWrapper>
+        <Button
+          className=" btn-outline-link p-4"
+          onClick={() => setEducationModalOpen(true)}
+        >
+          <>
+            <div className="display-4"> Education </div>
+            <i className="fas fa-graduation-cap fa-3x"></i>
+          </>
+        </Button>
+        <ModalWrapper
+          show={educationModalOpen}
+          onHide={() => setEducationModalOpen(false)}
+          modalId="educational-modal"
+          title="Education"
+        >
+          <EducationModalBody />
+        </ModalWrapper>
+      </div>
+    </PageLayout>
+  );
 };
