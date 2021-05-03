5c098c53ed0c4772d063e8e413cf2a3798496977
Deepanshu Kushwaha <deepanshu@atliq.com>
2021-05-03 20:49:54 +0530

    Added: ProjectPage component

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,36 @@
+import { FC } from "react";
+import { Card } from "react-bootstrap";
+
+interface ProjectCardProps {
+  url: string;
+  title: string;
+  description: string;
+  techStack: string;
+}
+
+export const ProjectCard: FC<ProjectCardProps> = ({
+  description,
+  title,
+  url,
+  techStack,
+}) => {
+  return (
+    <Card className="m-3 project-card">
+      <Card.Header>
+        <Card.Link href={url} target="_blank" rel="noreferrer">
+          <Card.Title className=" text-center m-0">{title}</Card.Title>
+        </Card.Link>
+      </Card.Header>
+      <Card.Title></Card.Title>
+      <Card.Body className="text-center">
+        <Card.Text className="display-8 text-weight-bolder ">
+          {description}
+        </Card.Text>
+
+        <Card.Text className="mx-5 tech-stack">
+          <span className="stack-text">{techStack}</span>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
