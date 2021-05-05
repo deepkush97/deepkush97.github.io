14e7cbda6729c6f45599885301619f1c8310b921
Deepanshu Kushwaha <deepanshu@atliq.com>
2021-05-06 01:43:35 +0530

    Updated: Uses preconnect to get the font instead of css import

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,17 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
+type Props = {};
 
+export default class CustomDocument extends Document<Props> {
   render() {
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Neucha&display=swap"
+            rel="stylesheet"
+          />
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
