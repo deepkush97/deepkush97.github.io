404dd3cded0626324795de1e6725dbb1ac019ac2
Deepanshu Kushwaha <kushwaha97deepanshu@gmail.com>
2021-08-18 20:35:39 +0530

    Added: Fonts and layout component

---
+++ src/configs/strings.ts
@@ -0,0 +1,5 @@
+export const site_name = "DeepKush";
+export const github_link = "https://github.com/deepkush97";
+export const title = "DeepKush";
+export const applicationDescription = "My portfolio website";
+export const locale = "en";
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ import Document, {
   NextScript,
   DocumentContext,
 } from "next/document";
+import { classes } from "../configs/classnames";
 
 export default class CustomDocument extends Document {
   static async getInitialProps(context: DocumentContext) {
@@ -16,21 +17,24 @@ export default class CustomDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta charSet="utf-8" />
+          <meta name="theme-color" content="#000000" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/apple-touch-icon.png"
+          />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Neucha&display=swap"
+            href="https://fonts.googleapis.com/css2?family=DM+Mono:wght@300;400;500&family=Recursive:wght@300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-          />
-          <meta name="theme-color" content="#000000" />
-          <link rel="apple-touch-icon" href="./apple-touch-icon.png" />
-          <link rel="icon" href="./favicon.ico" />
-          <link rel="manifest" href="./site.webmanifest" />
         </Head>
-        <body>
+        <body
+          className={`antialiased ${classes.background.light} ${classes.textBase}`}
+        >
           <Main />
           <NextScript />
         </body>
