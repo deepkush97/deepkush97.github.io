302460eeedfc6d3172f6858276dc77b4c39be046
Deepanshu Kushwaha <kushwaha97deepanshu@gmail.com>
2022-02-20 07:39:51 +0530

    Updated: Clickable ProjectCard for repoLink

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -4,7 +4,7 @@ import { classes } from "../configs/classnames";
 
 interface ProjectCardProps {
   url?: string;
-  repoLink?: string;
+  repoLink: string;
   title: string;
   description: string;
   techStack: string[];
@@ -18,48 +18,41 @@ export const ProjectCard: FC<ProjectCardProps> = ({
   techStack,
 }) => {
   return (
-    <div
-      className={`rounded shadow-xl mx-6 my-3 p-4 w-full md:w-1/3 flex flex-col h-60 ${classes.background.dark} hover:scale-105 duration-200`}
-    >
-      <div className="flex mb-4">
-        <span className={`flex-1 text-lg ${classes.textWhite}`}>{title}</span>
-        <div className="flex justify-start">
-          {repoLink && (
-            <Link href={repoLink}>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                className={`bg-transparent hover:bg-aquamarine-800 hover:bg-opacity-20 ${classes.textBase} ${classes.textHover} font-normal font-mono py-1 px-2 rounded`}
-              >
-                <i className="fab fa-github-alt"></i>
-              </a>
-            </Link>
-          )}
-          {url && (
-            <Link href={url}>
-              <a
-                target="_blank"
-                rel="noreferrer"
-                className={`bg-transparent hover:bg-aquamarine-800 hover:bg-opacity-20 ${classes.textBase} ${classes.textHover} font-normal font-mono py-1 px-2 rounded`}
-              >
-                <i className="fas fa-external-link-square-alt"></i>
-              </a>
-            </Link>
-          )}
+    <Link href={repoLink}>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        className={`rounded shadow-xl mx-6 my-3 p-4 w-full md:w-1/3 flex flex-col h-60 ${classes.background.dark} hover:scale-105 duration-200`}
+      >
+        <div className="flex mb-4">
+          <span className={`flex-1 text-lg ${classes.textWhite}`}>{title}</span>
+          <div className="flex justify-start">
+            {url && (
+              <Link href={url}>
+                <a
+                  title="Open live demo"
+                  className={`bg-transparent hover:bg-aquamarine-800 hover:bg-opacity-20 ${classes.textBase} ${classes.textHover} font-normal font-mono py-1 px-2 rounded`}
+                >
+                  <i className="fas fa-external-link-square-alt"></i>
+                </a>
+              </Link>
+            )}
+          </div>
         </div>
-      </div>
-      <p className="text-sm flex-1">{description}</p>
+        <p className="text-sm flex-1">{description}</p>
 
-      <p className="flex flex-wrap text-xs">
-        {techStack.map((stack, index) => (
-          <span
-            className={`bg-transparent ${classes.background.hoverTransparentAccent} ${classes.textAccent} inline-block font-normal cursor-default font-mono mr-1 mb-1 px-1 border ${classes.border}`}
-            key={index}
-          >
-            {stack}
-          </span>
-        ))}
-      </p>
-    </div>
+        <p className="flex flex-wrap text-xs gap-1">
+          {techStack.map((stack, index) => (
+            <div
+              className={`bg-transparent  ${classes.textAccent} inline-block font-normal font-mono  px-1 border ${classes.border}`}
+              title={stack}
+              key={index}
+            >
+              {stack}
+            </div>
+          ))}
+        </p>
+      </a>
+    </Link>
   );
 };
