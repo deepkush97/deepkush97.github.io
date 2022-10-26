7d5b0fcbb780bc8e8efd3c105ad8d9e40c799b7e
deepkush97 <kushwaha97deepanshu@gmail.com>
2022-10-26 06:37:06 +0000

    Deploying to gh-pages from @ deepkush97/deepkush97.github.io@fb288cb4cebdb1139e5c1212ddc529ebaa63dd1f 🚀

---
+++ _next/static/s9BvR6s6B62Si-9H296oc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-15334750be2a2012.js"],"/404":["static/chunks/pages/404-677e29b9fef71af2.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
